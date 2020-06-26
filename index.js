ea1cca01736e8e4f5e7af77f032e68f95b9eacb7
rezaulCSEDU <rezaul.csedu@gmail.com>
2020-06-26 17:34:19 +0600

    changed kick sound effect

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
 }
 function action() {
   var x=this.innerHTML;
+  if(x==="l"){
+    var a=new Audio("sounds/l.mp3");
+    a.play();
+    return;
+  }
+  else{
   var a=new Audio("sounds/"+x+".mp3");
   a.play();
+}
 }
