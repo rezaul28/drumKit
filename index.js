0a123b8111be22b905583836827142e9cbb3e513
rezaulCSEDU <rezaul.csedu@gmail.com>
2020-06-26 17:17:02 +0600

    plays specific sound with buttons

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", action);
 }
 function action() {
-  var a=new Audio("sounds/crash.mp3");
+  var x=this.innerHTML;
+  var a=new Audio("sounds/"+x+".mp3");
   a.play();
 }
