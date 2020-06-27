eb2392866397747fe1bb71bf3b69d161b6bc0e23
rezaulCSEDU <rezaul.csedu@gmail.com>
2020-06-27 16:32:48 +0600

    animation added

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", action);
 }
+
 function action() {
   var x=this.innerHTML;
-  var a=new Audio("sounds/"+x+".mp3");
-  a.play();
+  playMusic(x);
+  animation(x);
 }
+
 document.addEventListener("keydown", event => {
   var x=String.fromCharCode(event.keyCode);
   x=x.toLowerCase();
-  var a=new Audio("sounds/"+x+".mp3");
-  a.play();
+  playMusic(x);
+  animation(x);
 });
+
+function playMusic(key){
+  var a=new Audio("sounds/"+key+".mp3");
+  a.play();
+}
+
+function animation(key){
+  document.querySelector("."+key).classList.add("pressed");
+  setTimeout(function() {
+    document.querySelector("."+key).classList.remove("pressed");
+  }, 100);
+}
